c5879e1a81b40786d043161d26b66025dd35427d
Ungureanu Andrei <ungureanu.andrei85@gmail.com>
2018-11-19 18:54:15 +0200

    testing

--- index.php
+++ index.php
@@ -1,2 +1,94 @@
-<?php 
-echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@Ungureanu Cars</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-inverse navbar-fixed-top" >
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">@Ungureanu Cars</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+            </ul>
+            <form class="navbar-form navbar-right" action="/action_page.php">
+                <div class="login-container">
+                    <form action="/action_page.php">
+                        <input type="text" placeholder="Username" name="username" style="width: 100px">
+                        <input type="text" placeholder="Password" name="psw" style="width: 100px">
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </form>
+
+        </div>
+    </nav>
+
+</header>
+<br>
+<br>
+<br>
+<form>
+    <div class="container-fluid">
+
+        <div class="row" style="background-color:lightcyan;">
+            <div class="col-xs-4" style="height: 190px">
+            <h5> Skoda Octavia</h5>
+                <div>
+                    <img src="template/masina.jpg " class="img-fluid" style="height: 142px; width: 250px">
+                </div>
+                <div>
+                </div>
+            </div>
+            <div class="col-xs-4" style="height: 190px"> </div>
+            <div class="col-xs-4" style="height: 190px"> </div>
+        </div><div class="row" style="background-color:lightcyan;">
+            <div class="col-xs-4" style="height: 190px"> </div>
+            <div class="col-xs-4" style="height: 190px"> </div>
+            <div class="col-xs-4" style="height: 190px"> </div>
+        </div><div class="row" style="background-color:lightcyan;">
+            <div class="col-xs-4" style="height: 190px">
+            <h2 style="width: 100px">am reusit</h2>
+            </div>
+            <div class="col-xs-4" style="height: 190px"> </div>
+            <div class="col-xs-4" style="height: 190px"> </div>
+        </div>
+
+    </div>
+</form>
+
+
+<form>
+    <footer>
+        <style>
+            .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: black;
+                color: white;
+                text-align: center;
+            }
+        </style>
+        <div class="footer">
+            <p>
+                @drepturile de autor apartin @Andrei
+            </p>
+        </div>
+    </footer>
+</form>
+</body>
+</html>
+
